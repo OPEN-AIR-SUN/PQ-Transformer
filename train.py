7305e8532c0c699e67779ccf53979362bebe354c
c7w <admin@cc7w.cf>
2022-06-29 16:03:35 +0800

    revert train.py

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 from ast import dump
 
+import IPython
+
 from models.dump_helper import dump_results
 from models.dump_helper_quad import dump_results_quad
 
@@ -22,12 +24,12 @@ ROOT_DIR = BASE_DIR
 sys.path.append(os.path.join(ROOT_DIR, 'pointnet2'))
 sys.path.append(os.path.join(ROOT_DIR, 'models'))
 
+
 from utils.lr_scheduler import get_scheduler
 from utils.logger import setup_logger
 from models.pq_transformer import PQ_Transformer
 from models.loss_helper_pq import get_loss
-from models.ap_helper_pq import APCalculator, parse_predictions, parse_groundtruths, QUADAPCalculator, \
-    parse_quad_predictions, parse_quad_groundtruths
+from models.ap_helper_pq import APCalculator, parse_predictions, parse_groundtruths,QUADAPCalculator, parse_quad_predictions,parse_quad_groundtruths
 
 
 def parse_option():
@@ -71,7 +73,7 @@ def parse_option():
                         choices=["step", "cosine"], help="learning rate scheduler")
     parser.add_argument('--warmup-epoch', type=int, default=-1, help='warmup epoch')
     parser.add_argument('--warmup-multiplier', type=int, default=100, help='warmup multiplier')
-    parser.add_argument('--lr_decay_epochs', type=int, default=[900, 1000], nargs='+',
+    parser.add_argument('--lr_decay_epochs', type=int, default=[900,1000], nargs='+',
                         help='for step scheduler. where to decay lr, can be a list')
     parser.add_argument('--lr_decay_rate', type=float, default=0.1,
                         help='for step scheduler. decay rate for learning rate')
@@ -89,7 +91,7 @@ def parse_option():
 
     # others
     parser.add_argument("--local_rank", type=int, help='local rank for DistributedDataParallel')
-    parser.add_argument('--ap_iou_thresholds', type=float, default=[0.25], nargs='+',  # 0.5
+    parser.add_argument('--ap_iou_thresholds', type=float, default=[0.25], nargs='+',  #0.5
                         help='A list of AP IoU thresholds [default: 0.25,0.5]')
     parser.add_argument("--rng_seed", type=int, default=0, help='manual seed')
     parser.add_argument("--pc_loss", action='store_true', help='pc_loss')
@@ -110,7 +112,7 @@ def initiate_environment(args):
     torch.cuda.manual_seed_all(args.rng_seed)
     np.random.seed(args.rng_seed)
     random.seed(args.rng_seed)
-
+    
 
 def load_checkpoint(args, model, optimizer, scheduler):
     logger.info("=> loading checkpoint '{}'".format(args.checkpoint_path))
@@ -173,7 +175,7 @@ def get_loader(args):
                                                 use_color=True if args.use_color else False,
                                                 use_height=True if args.use_height else False,
                                                 start_proportion=args.start_proportion,
-                                                end_proportion=args.end_proportion, )
+                                                end_proportion=args.end_proportion,)
 
         TEST_DATASET = ScannetDetectionDataset('val', num_points=args.num_point,
                                                augment=False,
@@ -214,14 +216,14 @@ def get_model(args, DATASET_CONFIG):
     else:
         num_input_channel = int(args.use_color) * 3
     model = PQ_Transformer(num_class=DATASET_CONFIG.num_class,
-                           num_heading_bin=DATASET_CONFIG.num_heading_bin,
-                           num_size_cluster=DATASET_CONFIG.num_size_cluster,
-                           mean_size_arr=DATASET_CONFIG.mean_size_arr,
-                           input_feature_dim=num_input_channel,
-                           num_proposal=args.num_target,
-                           num_quad_proposal=args.quad_num_target,
-                           sampling=args.sampling
-                           )
+                              num_heading_bin=DATASET_CONFIG.num_heading_bin,
+                              num_size_cluster=DATASET_CONFIG.num_size_cluster,
+                              mean_size_arr=DATASET_CONFIG.mean_size_arr,
+                              input_feature_dim=num_input_channel,
+                              num_proposal=args.num_target,
+                              num_quad_proposal=args.quad_num_target,
+                              sampling=args.sampling
+                              )
     criterion = get_loss
     return model, criterion
 
@@ -262,7 +264,7 @@ def main(args):
     # Used for AP calculation
     CONFIG_DICT = {'remove_empty_box': False, 'use_3d_nms': True,
                    'nms_iou': 0.25, 'use_old_type_nms': False, 'cls_nms': True,
-                   'per_class_proposal': True, 'conf_thresh': 0.0, 'quad_thresh': 0.5,
+                   'per_class_proposal': True, 'conf_thresh': 0.0,'quad_thresh':0.5,
                    'dataset_config': DATASET_CONFIG}
 
     for epoch in range(args.start_epoch, args.max_epoch + 1):
@@ -278,12 +280,12 @@ def main(args):
                                                                optimizer.param_groups[1]['lr']))
 
         if epoch % args.val_freq == 1:
-            evaluate_one_epoch(test_loader, DATASET_CONFIG, CONFIG_DICT, args.ap_iou_thresholds, model, criterion, args)
-
+            evaluate_one_epoch(test_loader, DATASET_CONFIG, CONFIG_DICT, args.ap_iou_thresholds, model,criterion, args) 
+              
         if dist.get_rank() == 0:
             # save model
             save_checkpoint(args, epoch, model, optimizer, scheduler)
-
+    
     evaluate_one_epoch(test_loader, DATASET_CONFIG, CONFIG_DICT, args.ap_iou_thresholds, model, criterion, args)
     save_checkpoint(args, 'last', model, optimizer, scheduler, save_cur=True)
     logger.info("Saved in {}".format(os.path.join(args.log_dir, f'ckpt_epoch_last.pth')))
@@ -299,39 +301,16 @@ def train_one_epoch(epoch, train_loader, DATASET_CONFIG, model, criterion, optim
             if key == 'scan_name':
                 continue
             batch_data_label[key] = batch_data_label[key].cuda(non_blocking=True)
-
-
-        # Randomly pick 10% of the data in range(self.num_points)
-        bs = batch_data_label['point_clouds'].shape[0]
-
-        labelled_indices, unlabelled_indices = [], []
-
-        for b in range(bs):
-            scene_idx = int(batch_data_label['scan_name'][b][5:9])
-            np.random.seed(scene_idx * scene_idx + 3)  # f: scene_idx -> random seed, for reproducing the results later
-            # TODO: what if we use FPS to pick 10% of the data?
-            labelled_indices.append(
-                np.random.choice(train_loader.dataset.num_points, \
-                                 int(train_loader.dataset.num_points * 0.1), replace=False)
-            )
-            unlabelled_indices.append(
-                np.setdiff1d(np.arange(train_loader.dataset.num_points), labelled_indices[b])
-            )
-
-        labelled_indices = np.stack(labelled_indices)
-        unlabelled_indices = np.stack(unlabelled_indices)
-
-        former_point_clouds = batch_data_label['point_clouds']
-        inputs = {'point_clouds': torch.stack([former_point_clouds[b][labelled_indices[b], :] for b in range(bs)])}
+        inputs = {'point_clouds': batch_data_label['point_clouds']}
 
         # Forward pass
         end_points = model(inputs)
-
+ 
         # Compute loss and gradients, update parameters.
         for key in batch_data_label:
             assert (key not in end_points)
             end_points[key] = batch_data_label[key]
-        loss, end_points = criterion(end_points, DATASET_CONFIG, pc_loss=config.pc_loss)
+        loss, end_points = criterion(end_points, DATASET_CONFIG, pc_loss = config.pc_loss)
 
         optimizer.zero_grad()
         loss.backward()
@@ -371,17 +350,20 @@ def train_one_epoch(epoch, train_loader, DATASET_CONFIG, model, criterion, optim
 
 
 def evaluate_one_epoch(test_loader, DATASET_CONFIG, CONFIG_DICT, AP_IOU_THRESHOLDS, model, criterion, config):
+    
     stat_dict = {}
 
+
     if config.num_decoder_layers > 0:
-        prefixes = ['last_']  # , 'proposal_'] + [f'{i}head_' for i in range(config.num_decoder_layers - 1)]
+        prefixes = ['last_'] #, 'proposal_'] + [f'{i}head_' for i in range(config.num_decoder_layers - 1)]
     else:
         prefixes = ['proposal_']  # only proposal
     ap_calculator_list = [APCalculator(iou_thresh, DATASET_CONFIG.class2type) \
                           for iou_thresh in AP_IOU_THRESHOLDS]
-
+    
     quad_ap_calculator_list = [QUADAPCalculator(iou_thresh, DATASET_CONFIG.class2quad) \
-                               for iou_thresh in AP_IOU_THRESHOLDS]
+                          for iou_thresh in AP_IOU_THRESHOLDS]
+    
 
     mAPs = [[iou_thresh, {k: 0 for k in prefixes}] for iou_thresh in AP_IOU_THRESHOLDS]
 
@@ -400,20 +382,24 @@ def evaluate_one_epoch(test_loader, DATASET_CONFIG, CONFIG_DICT, AP_IOU_THRESHOL
         for key in batch_data_label:
             if key == 'scan_name':
                 continue
-
+            
             batch_data_label[key] = batch_data_label[key].cuda(non_blocking=True)
-
+        
         # Forward pass
         inputs = {'point_clouds': batch_data_label['point_clouds']}
         with torch.no_grad():
             end_points = model(inputs)
 
+        import IPython
+        IPython.embed("in training process, before calcing loss")
+
         # Compute loss
         for key in batch_data_label:
             assert (key not in end_points)
             end_points[key] = batch_data_label[key]
+        
 
-        loss, end_points = criterion(end_points, DATASET_CONFIG, pc_loss=config.pc_loss)
+        loss, end_points = criterion(end_points, DATASET_CONFIG, pc_loss = config.pc_loss)
 
         # Accumulate statistics and print out
         for key in end_points:
@@ -429,26 +415,26 @@ def evaluate_one_epoch(test_loader, DATASET_CONFIG, CONFIG_DICT, AP_IOU_THRESHOL
             batch_gt_map_cls = parse_groundtruths(end_points, CONFIG_DICT)
             batch_pred_map_cls_dict[prefix].append(batch_pred_map_cls)
             batch_gt_map_cls_dict[prefix].append(batch_gt_map_cls)
-            end_points['pred_mask'] = pred_mask
+            end_points['pred_mask']=pred_mask
 
-            batch_pred_quad_map_cls, pred_quad_mask, batch_pred_quad_corner = parse_quad_predictions(end_points,
-                                                                                                     CONFIG_DICT,
-                                                                                                     prefix)
-            batch_gt_quad_map_cls, batch_gt_quad_corner = parse_quad_groundtruths(end_points, CONFIG_DICT)
 
+            batch_pred_quad_map_cls,pred_quad_mask,batch_pred_quad_corner = parse_quad_predictions(end_points, CONFIG_DICT, prefix)
+            batch_gt_quad_map_cls,batch_gt_quad_corner = parse_quad_groundtruths(end_points, CONFIG_DICT)
+            
             batch_pred_quad_map_cls_dict[prefix].append(batch_pred_quad_map_cls)
             batch_gt_quad_map_cls_dict[prefix].append(batch_gt_quad_map_cls)
             batch_pred_corner_dict[prefix].append(batch_pred_quad_corner)
             batch_gt_corner_dict[prefix].append(batch_gt_quad_corner)
+            
+            batch_gt_horizontal_dict[prefix].append(end_points['horizontal_quads']) 
 
-            batch_gt_horizontal_dict[prefix].append(end_points['horizontal_quads'])
-
-            end_points['pred_quad_mask'] = pred_quad_mask
-
+            end_points['pred_quad_mask']=pred_quad_mask
+            
             if config.dump_result:
                 print("dumping...")
-                dump_results(end_points, os.path.join(ROOT_DIR, 'dump/%01dbest' % (batch_idx)), DATASET_CONFIG)
-                dump_results_quad(end_points, os.path.join(ROOT_DIR, 'dump/%01dbest' % (batch_idx)), DATASET_CONFIG)
+                dump_results(end_points, os.path.join(ROOT_DIR,'dump/%01dbest'%(batch_idx)), DATASET_CONFIG) 
+                dump_results_quad(end_points, os.path.join(ROOT_DIR,'dump/%01dbest'%(batch_idx)), DATASET_CONFIG) 
+            
 
         if (batch_idx + 1) % config.print_freq == 0:
             logger.info(f'Eval: [{batch_idx + 1}/{len(test_loader)}]  ' + ''.join(
@@ -464,16 +450,16 @@ def evaluate_one_epoch(test_loader, DATASET_CONFIG, CONFIG_DICT, AP_IOU_THRESHOL
             for ihead in range(config.num_decoder_layers - 2, -1, -1):
                 logger.info(''.join([f'{key} {stat_dict[key] / (float(batch_idx + 1)):.4f} \t'
                                      for key in sorted(stat_dict.keys()) if f'{ihead}head_' in key]))
-    # objects:
+    #objects:
     mAP = 0.0
     for prefix in prefixes:
         for (batch_pred_map_cls, batch_gt_map_cls) in zip(batch_pred_map_cls_dict[prefix],
                                                           batch_gt_map_cls_dict[prefix]):
-
+        
             for ap_calculator in ap_calculator_list:
                 ap_calculator.step(batch_pred_map_cls, batch_gt_map_cls)
         # Evaluate average precision
-
+        
         for i, ap_calculator in enumerate(ap_calculator_list):
             metrics_dict = ap_calculator.compute_metrics()
             logger.info(f'=====================>{prefix} IOU THRESH: {AP_IOU_THRESHOLDS[i]}<=====================')
@@ -488,17 +474,14 @@ def evaluate_one_epoch(test_loader, DATASET_CONFIG, CONFIG_DICT, AP_IOU_THRESHOL
         logger.info(f'IoU[{mAP[0]}]:\t' + ''.join([f'{key}: {mAP[1][key]:.4f} \t' for key in sorted(mAP[1].keys())]))
 
     object_map = mAP[1]['last_']
-    # quad
+    #quad
     mAP_ = 0.0
-
+    
     for prefix in prefixes:
-        for (batch_pred_map_cls, batch_gt_map_cls, batch_pred_corner, batch_gt_corner, batch_gt_horizontal) in zip(
-                batch_pred_quad_map_cls_dict[prefix],
-                batch_gt_quad_map_cls_dict[prefix], batch_pred_corner_dict[prefix], batch_gt_corner_dict[prefix],
-                batch_gt_horizontal_dict[prefix]):
+        for (batch_pred_map_cls, batch_gt_map_cls,batch_pred_corner,batch_gt_corner,batch_gt_horizontal) in zip(batch_pred_quad_map_cls_dict[prefix],
+                                                          batch_gt_quad_map_cls_dict[prefix],batch_pred_corner_dict[prefix],batch_gt_corner_dict[prefix],batch_gt_horizontal_dict[prefix]):
             for ap_calculator in quad_ap_calculator_list:
-                ap_calculator.step(batch_pred_map_cls, batch_gt_map_cls, batch_pred_corner, batch_gt_corner,
-                                   batch_gt_horizontal)
+                ap_calculator.step(batch_pred_map_cls, batch_gt_map_cls,batch_pred_corner,batch_gt_corner,batch_gt_horizontal)
         # Evaluate average precision
         for i, ap_calculator in enumerate(quad_ap_calculator_list):
             metrics_dict = ap_calculator.compute_metrics()
@@ -515,20 +498,21 @@ def evaluate_one_epoch(test_loader, DATASET_CONFIG, CONFIG_DICT, AP_IOU_THRESHOL
 
     for mAP_ in mAPs:
         logger.info(f'IoU[{mAP_[0]}]:\t' + ''.join([f'{key}: {mAP_[1][key]:.4f} \t' for key in sorted(mAP_[1].keys())]))
-
+ 
     return mAP
 
 
 if __name__ == '__main__':
     opt = parse_option()
-
+    
+    
     torch.cuda.set_device(opt.local_rank)
     torch.distributed.init_process_group(backend='nccl', init_method='env://')
     initiate_environment(opt)
     torch.backends.cudnn.enabled = True
     torch.backends.cudnn.benchmark = True
     torch.backends.cudnn.deterministic = True
-
+    
     LOG_DIR = os.path.join(opt.log_dir, 'pq-transformer',
                            f'{opt.dataset}_{int(time.time())}', f'{np.random.randint(100000000)}')
     while os.path.exists(LOG_DIR):
